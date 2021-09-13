e02ba526ffa8c8ab21ff4c9ad968145536f5fd56
Flavio Pires <flavioasp77@gmail.com>
2021-09-13 15:19:42 -0300

    Requisitos 1, 2 e 3 finalizados

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import About from './About';
+import { Route } from 'react-router-dom';
 
 class Home extends Component {
   render() {
@@ -6,6 +8,8 @@ class Home extends Component {
       <div>
         <h2>Home</h2>
         <p> My awesome Home component </p>
+        <Route path="/" component={ Home } />
+        <Route path="/about" component={ About } />
       </div>
     );
   }
